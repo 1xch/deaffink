27671fc6385fe66f4acc0ef53ad14e44525d77a1
blueblank <blueblank@gmail.com>
2018-07-11 12:50:16 -0400

    odds&ends

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 DEAFFINK
 
-Drop in configuration.
+Drop in configuration with one command.
+
+deaffink is one file compiled to binary that writes out a file called "configuration.go" in the directory it is invoked.
+
+Configure anything you require with any number of functions you create.
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 // deaffink is one file compiled to binary that writes out a file called
 // "configuration.go" in the directory it is invoked.
 //
-// Configure anything you require with any number of functions you create.
+// Configure anything you require with any number of functions you add.
 package main
 
 import (
